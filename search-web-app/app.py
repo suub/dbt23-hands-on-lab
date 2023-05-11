d553dfbe4b992bf06010aca9ec8ebca5c7f82561
flug <marie-saphira.flug@suub.uni-bremen.de>
2023-05-11 15:00:52 +0200

    add abstract to search app

--- search-web-app/app.py
+++ search-web-app/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 BASE_PATH='http://solr:8983/solr/workshop/select?wt=json&df=name&rows=250&q='
 DB_CONN='postgresql://nw:nw@nightwatch-db:5432/nw'
 
-SOLR_STRING_FIELDS = ["title", "author", "title_authors"]
+SOLR_STRING_FIELDS = ["title", "author", "title_authors", "abstract"]
 SOLR_INT_FIELDS = ["year"]
 RECORDS_TABLE_COLUMNS = [
     "access_options",
@@ -20,7 +20,8 @@ RECORDS_TABLE_COLUMNS = [
     "identifiers",
     "publication_date",
     "source",
-    "title"
+    "title",
+    "abstract"
 ]
 DB_QUERY = "SELECT * FROM records WHERE id = $1);"
 
